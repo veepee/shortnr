dfbbb49a3c93b0a4467139d1ad1520e39dd1e2ea
veepee <villepekkah@gmail.com>
2015-02-08 08:04:15 -0500

    POST request parsing

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function(req, res) {
   res.send('shortnr - yet another url shortening service');
 });
 
 app.post('/shorten', function(req, res) {
-  res.send('shorten');
+  res.send('got ' + req.body.link);
 });
 
 app.get('/:id', function(req, res) {
