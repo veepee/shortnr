e5b21b8855dcd54d5b2ef3c189006dfa76009d65
veepee <villepekkah@gmail.com>
2015-02-08 07:57:06 -0500

    Added the routes

--- app.js
+++ app.js
@@ -2,7 +2,15 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
-  res.send('Hello world!');
+  res.send('shortnr - yet another url shortening service');
+});
+
+app.post('/shorten', function(req, res) {
+  res.send('shorten');
+});
+
+app.get('/:id', function(req, res) {
+  res.send('got ' + req.params.id);
 });
 
 module.exports = app;
